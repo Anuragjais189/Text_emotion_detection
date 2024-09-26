c71873fc17ba17147764a070eb816ac1c89f090b
Anuragjais189 <76873184+Anuragjais189@users.noreply.github.com>
2024-09-26 06:50:37 +0000

    added the first codetab removed pylance and added test_env

---
+++ test_env.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import keras 
+import tensorflow as tf
+import sklearn as sk
+from sklearn.model_selection import train_test_split
+
+print("Pandas version:", pd.__version__)
+print("NumPy version:", np.__version__)
+print("Keras version:", keras.__version__)
+print("TensorFlow version:", tf.__version__)
